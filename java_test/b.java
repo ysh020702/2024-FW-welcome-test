7fa7257037eaca461f9b778078d8728f3340c527
ysh020702 <ysh020702@gmail.com>
2024-08-21 14:48:56 +0900

    b

--- java_test/b.java
+++ java_test/b.java
@@ -1,9 +1,21 @@
 package java_test;
 
+import java.util.Scanner;
+
 public class b {
     public static void main(String[] args) {
 
-        // 이 부분에 code를 작성해주세요!
+        Scanner scanner = new Scanner(System.in);
+
+        char first = scanner.nextLine().charAt(0);
+        String target = "aeiou";
+
+        if(target.indexOf(first) == -1){
+            System.out.println('X');
+        }else{
+            System.out.println("O");
+        }
+        
 
     }
 
