216b4b6a32563afecb6367a92cdc6be83fc3a38f
ysh020702 <ysh020702@gmail.com>
2024-08-21 14:58:35 +0900

    4번

--- java_test/d.java
+++ java_test/d.java
@@ -1,10 +1,21 @@
 package java_test;
 
+import java.util.Scanner;
+
 public class d {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
-        // 이 부분에 code를 작성해주세요!
+        int n = scanner.nextInt();
 
+        while(n <= 0){
+            System.out.println('X');
+            n = scanner.nextInt();
+        }
+
+        System.out.println(n*(n+1)/2);
+
+        scanner.close();
     }
 
 }
