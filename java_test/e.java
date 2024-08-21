68d695e526ec02fdee90aa80dccbd22af6805d36
ysh020702 <ysh020702@gmail.com>
2024-08-21 15:10:48 +0900

    e번

--- java_test/e.java
+++ java_test/e.java
@@ -1,10 +1,31 @@
 package java_test;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
 public class e {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
-        // 이 부분에 code를 작성해주세요!
+        Set<Integer> thirties = new HashSet<>(Arrays.asList(4,6,9,11));
+        int year, month;
 
+        year = scanner.nextInt();
+        month = scanner.nextInt();
+
+        boolean isYoon = ((year % 4) == 0 && (year % 100) != 0) || (year % 400) == 0;
+
+        if(month == 2){
+            if(isYoon) System.out.println(29);
+            else System.out.println(28);
+        } else {
+            if(thirties.contains(month)) System.out.println(30);
+            else System.out.println(31);
+        }
+
+        scanner.close();
     }
 
 }
